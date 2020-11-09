dcd83df769bf599948d3b09f99a3be1b34c8151c
Andrea Rossi <a.rossi.andrea@gmail.com>
2020-11-09 18:34:20 +0100

    Field Voxel preview

---
+++ src/ghComp/Wasp_Field Iso Voxels.py
@@ -0,0 +1,102 @@
+# Wasp: Discrete Design with Grasshopper plug-in (GPL) initiated by Andrea Rossi
+# 
+# This file is part of Wasp.
+# 
+# Copyright (c) 2017, Andrea Rossi <a.rossi.andrea@gmail.com>
+# Wasp is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published 
+# by the Free Software Foundation; either version 3 of the License, 
+# or (at your option) any later version. 
+# 
+# Wasp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Wasp; If not, see <http://www.gnu.org/licenses/>.
+# 
+# @license GPL-3.0 <https://www.gnu.org/licenses/gpl.html>
+#
+# Significant parts of Wasp have been developed by Andrea Rossi
+# as part of research on digital materials and discrete design at:
+# DDU Digital Design Unit - Prof. Oliver Tessmann
+# Technische Universitt Darmstadt
+
+
+#########################################################################
+##                            COMPONENT INFO                           ##
+#########################################################################
+
+"""
+Draws isolines across a field object
+--> WIP Component: might be incomplete or contain bugs! <--
+-
+Provided by Wasp 0.4
+    Args:
+        FIELD: Field object to extract isolines from
+        PLN: Plane for isolines extraction (0: XY Plane, 1: YZ Plane, 2: XZ Plane)
+        T: Z parameter where to extract isolines
+        ISO: Isolevel of the curves
+    Returns:
+        CRV: Isocurves
+"""
+
+ghenv.Component.Name = "Wasp_Field Iso Voxels"
+ghenv.Component.NickName = 'FieldVox'
+ghenv.Component.Message = 'VER 0.4.007'
+ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
+ghenv.Component.Category = "Wasp"
+ghenv.Component.SubCategory =  "5 | Fields"
+try: ghenv.Component.AdditionalHelpFromDocStrings = "1"
+except: pass
+
+
+import sys
+import Rhino.Geometry as rg
+import Grasshopper as gh
+import math
+
+
+## add Wasp install directory to system path
+wasp_loaded = False
+ghcompfolder = gh.Folders.DefaultAssemblyFolder
+if ghcompfolder not in sys.path:
+    sys.path.append(ghcompfolder)
+try:
+    from wasp import __version__
+    wasp_loaded = True
+except:
+    msg = "Cannot import Wasp. Is the wasp folder available in " + ghcompfolder + "?"
+    ghenv.Component.AddRuntimeMessage(gh.Kernel.GH_RuntimeMessageLevel.Error, msg)
+
+## if Wasp is installed correctly, load the classes required by the component
+if wasp_loaded:
+    pass
+
+
+
+def main(field, iso):
+    check_data = True
+    
+    ##check inputs
+    if field is None:
+        check_data = False
+        msg = "No field provided"
+        ghenv.Component.AddRuntimeMessage(gh.Kernel.GH_RuntimeMessageLevel.Warning, msg)
+    
+    if iso is None:
+        check_data = False
+        msg = "No isolevel provided"
+        ghenv.Component.AddRuntimeMessage(gh.Kernel.GH_RuntimeMessageLevel.Warning, msg)
+    
+    if check_data:
+        
+        return field.compute_voxel_mesh(iso)
+    else:
+        return -1
+
+result = main(FIELD, ISO)
+
+if result != -1:
+    VM = result
