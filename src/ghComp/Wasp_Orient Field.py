dcad2678a8a6cb241be9bcef5dfef3281007d52d
Andrea Rossi <a.rossi.andrea@gmail.com>
2020-11-11 11:48:07 +0100

    added orient field component + icons fixes

--- src/ghComp/Wasp_Orient Field.py
+++ src/ghComp/Wasp_Orient Field.py
@@ -29,29 +29,28 @@
 #########################################################################
 
 """
-Compute a voxel-mesh of the given field at the chosen iso-level.
+Changes the base plane of the field, with the effect of creating a transformed copy.
 -
 Provided by Wasp 0.4
     Args:
-        FIELD: Field object to extract iso-voxels from
-        ISO: Isolevel of the voxel mesh
+        FIELD: Field to be transformed
+        PLN: Target plane
     Returns:
-        VM: Voxel mesh at the chosen iso-level
+        F_TR: Transformed field
 """
 
-ghenv.Component.Name = "Wasp_Field Iso Voxels"
-ghenv.Component.NickName = 'FieldVox'
+ghenv.Component.Name = "Wasp_Orient Field"
+ghenv.Component.NickName = 'FieldOrient'
 ghenv.Component.Message = 'VER 0.4.007'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Wasp"
-ghenv.Component.SubCategory =  "5 | Fields"
-try: ghenv.Component.AdditionalHelpFromDocStrings = "2"
+ghenv.Component.SubCategory = "5 | Fields"
+try: ghenv.Component.AdditionalHelpFromDocStrings = "1"
 except: pass
 
-
 import sys
-import Rhino.Geometry as rg
 import Grasshopper as gh
+import Rhino.Geometry as rg
 import math
 
 
@@ -69,31 +68,42 @@ except:
 
 ## if Wasp is installed correctly, load the classes required by the component
 if wasp_loaded:
-    pass
+    from wasp.core import Part
 
 
-
-def main(field, iso):
+def main(field, target_plane):
+    
     check_data = True
     
     ##check inputs
     if field is None:
         check_data = False
-        msg = "No field provided"
+        msg = "Please provide a valid field to be transformed"
         ghenv.Component.AddRuntimeMessage(gh.Kernel.GH_RuntimeMessageLevel.Warning, msg)
     
-    if iso is None:
+    if target_plane is None:
         check_data = False
-        msg = "No isolevel provided"
+        msg = "Please provide a valid transformation"
         ghenv.Component.AddRuntimeMessage(gh.Kernel.GH_RuntimeMessageLevel.Warning, msg)
     
     if check_data:
+        ## create a plane-to-plane transformation
+        orient_transform = rg.Transform.PlaneToPlane(field.plane, target_plane)
+        ## transform part
+        field_trans = field.transform(orient_transform)
         
-        return field.compute_voxel_mesh(iso)
+        ## flip part if negative scaling occurs
+        if orient_transform.M00 * orient_transform.M11 * orient_transform.M22 < 0:
+            ## geometry
+            for bou in field_trans.boundaries:
+                bou.Flip(True, True, True)
+        
+        return field_trans
     else:
         return -1
 
-result = main(FIELD, ISO)
+
+result = main(FIELD, PLN)
 
 if result != -1:
-    VM = result
+    F_TR = result
