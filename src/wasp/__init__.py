a309bc13eb48db571742696cf1436068e1392d62
Andrea Rossi <a.rossi.andrea@gmail.com>
2021-03-22 12:15:49 +0100

    new icons + version renumbering

--- src/wasp/__init__.py
+++ src/wasp/__init__.py
@@ -4,7 +4,7 @@
 This file is part of Wasp. https://github.com/ar0551/Wasp
 @license GPL-3.0 <https://www.gnu.org/licenses/gpl.html>
 
-@version 0.4.015
+@version 0.5.001
 
 Wasp: Discrete Design with Grasshopper plug-in (GPL) initiated by Andrea Rossi
  
@@ -44,6 +44,6 @@ __author__ = ['ar0551 <a.rossi.andrea@gmail.com>']
 __copyright__ = 'Copyright 2017-2021 - Andrea Rossi'
 __license__ = 'GPLv3.0'
 __email__ = 'ghwasp@gmail.com'
-__version__ = '0.4.015'
+__version__ = '0.5.001'
 
 
